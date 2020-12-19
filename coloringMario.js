f265203ec076bdb51ffbddf7c629724e4b75787b
KateChoo <twodoor1993@gmail.com>
2020-12-19 19:21:26 +0800

    xmas coloring

--- coloringMario.js
+++ coloringMario.js
@@ -2,37 +2,38 @@ const width = 10
 const grid = document.querySelector('.grid')
 let squares = []
 
-// 0 - red
+// 0 - blue
 // 1 - background_color
 // 2 - gold
 // 3 - skin
 // 4 - empty
 
 const layout = [
-1, 1, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 4,  
-1, 1, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 4, 4, 4, 
-1, 1, 4, 4, 4, 2, 2, 2, 2, 3, 3, 2, 3, 4, 4, 4, 4, 4, 4, 4,  
-1, 1, 4, 4, 2, 2, 3, 2, 3, 3, 3, 2, 3, 3, 3, 4, 4, 4, 4, 4,  
-1, 1, 4, 4, 2, 2, 3, 2, 2, 3, 3, 3, 2, 3, 3, 3, 4, 4, 4, 4,  
-1, 1, 4, 4, 2, 2, 2, 3, 3, 3, 3, 2, 2, 2, 2, 4, 4, 4, 4, 4,  
-1, 1, 4, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4,  
-1, 1, 4, 4, 4, 2, 2, 2, 0, 2, 2, 2, 4, 4, 4, 4, 4, 4, 4, 4,  
-1, 1, 4, 4, 2, 2, 2, 2, 0, 2, 2, 0, 2, 2, 2, 4, 4, 4, 4, 4, 
-1, 1, 4, 2, 2, 2, 2, 2, 0, 0, 0, 0, 2, 2, 2, 2, 4, 4, 4, 4,  
-1, 1, 4, 3, 3, 3, 2, 0, 3, 0, 0, 3, 0, 2, 3, 3, 4, 4, 4, 4,  
-1, 1, 4, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 3, 3, 3, 4, 4, 4, 4,  
-1, 1, 4, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 4, 4, 4, 4,  
-1, 1, 4, 4, 4, 0, 0, 0, 0, 4, 0, 0, 0, 0, 4, 4, 4, 4, 4, 4,  
-1, 1, 4, 4, 2, 2, 2, 2, 4, 4, 4, 2, 2, 2, 2, 4, 4, 4, 4, 4,
-1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,   
-1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,  
-1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,  
-1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,  
-1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4  
+1, 1, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 4, 4, 4, 4, 4, 4, 1, 1,  
+1, 1, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 4, 1, 1, 
+1, 1, 4, 4, 4, 2, 2, 2, 2, 3, 3, 2, 3, 4, 4, 4, 4, 4, 1, 1,  
+1, 1, 4, 4, 2, 2, 3, 2, 3, 3, 3, 2, 3, 3, 3, 4, 4, 4, 1, 1,  
+1, 1, 4, 4, 2, 2, 3, 2, 2, 3, 3, 3, 2, 3, 3, 3, 4, 4, 1, 1,  
+1, 1, 4, 4, 2, 2, 2, 3, 3, 3, 3, 2, 2, 2, 2, 4, 4, 4, 1, 1,  
+1, 1, 4, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 1, 1,  
+1, 1, 4, 4, 4, 2, 2, 2, 0, 2, 2, 2, 4, 4, 4, 4, 4, 4, 1, 1,  
+1, 1, 4, 4, 2, 2, 2, 2, 0, 2, 2, 0, 2, 2, 2, 4, 4, 4, 1, 1, 
+1, 1, 4, 2, 2, 2, 2, 2, 0, 0, 0, 0, 2, 2, 2, 2, 4, 4, 1, 1,  
+1, 1, 4, 3, 3, 3, 2, 0, 3, 0, 0, 3, 0, 2, 3, 3, 4, 4, 1, 1,  
+1, 1, 4, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 3, 3, 3, 4, 4, 1, 1,  
+1, 1, 4, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 4, 4, 1, 1,  
+1, 1, 4, 4, 4, 0, 0, 0, 0, 4, 0, 0, 0, 0, 4, 4, 4, 4, 1, 1,  
+1, 1, 4, 4, 2, 2, 2, 2, 4, 4, 4, 2, 2, 2, 2, 4, 4, 4, 1, 1,
+1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1,   
+1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1,  
+1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1,  
+1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1,  
+1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1  
 ]
 
 const xmasEmoji = [
-    '🎅', '❄️', '🎁', '🦌', '⛄', '👪', '🎄',
+    '🎅', '❄️', '🎁', '🦌', '⛄', '🎄', 'Merry', 'Xmas'
+    // '👪',
 ]
 
 const xmasBlessing = [
@@ -40,8 +41,7 @@ const xmasBlessing = [
     'Merry Xmas', '', '', '', '', '', '', '','Happy', '', '', '', '', '', '', '', '','2021'
 ]
 
-
-function createBoard() { 
+function createBoard() {
     for (let i = 0; i < layout.length; i++) {
         const square = document.createElement('div')
         grid.appendChild(square)
@@ -54,7 +54,7 @@ function createBoard() {
         } else if (layout[i] === 3){
             squares[i].classList.add('skin')
         } else if (layout[i] === 0){
-            squares[i].classList.add('red')
+            squares[i].classList.add('blue')
         }     
     }
 }
@@ -62,9 +62,9 @@ createBoard()
 
 function marioColorChange(e){
     //console.log(e.target)
-    if(e.target.className === 'red'){
-        e.target.classList.remove('red')
-        e.target.classList.add('blue')
+    if(e.target.className === 'blue'){
+        e.target.classList.remove('blue')
+        e.target.classList.add('red')
 
         //let randomEmoji = Math.floor(Math.random() * xmasEmoji.length);
         //e.target.textContent = xmasEmoji[randomEmoji];
@@ -74,18 +74,15 @@ function marioColorChange(e){
         e.target.classList.remove('background_color')
         e.target.classList.add('change_background')
         let randomEmoji = Math.floor(Math.random() * xmasEmoji.length);
-        e.target.textContent = xmasEmoji[randomEmoji];
-        // for (let i = 0; i < xmasBlessing.length; ++i){
-        //     console.log(i)
-        //     e.target.style.backgroundImage = xmasBlessing[i];
-            
-        // }
-        // xmasBlessing.forEach(index =>
-        //     e.target.textContent = xmasBlessing[index] 
-        //     //console.log(e.target.textContent = xmasBlessing[index])
-        // )
-        
-    }
+        e.target.textContent = xmasEmoji[randomEmoji];        
+    } else if (e.target.className === 'gold'){
+        e.target.classList.remove('gold')
+        e.target.classList.add('green')
+    } 
+    // else if (e.target.className === 'skin'){
+    //     e.target.classList.remove('skin')
+    //     e.target.classList.add('white')
+    // }
 }
 
 function marioColorChange2(e){
